07ac2b4275b7e69d4f62bcc32bc9634c76279704
AleksKostadinov <kostadinov12@gmail.com>
2023-07-31 09:44:49 +0300

    Added Variation model

--- shop_app/models.py
+++ shop_app/models.py
@@ -58,5 +58,27 @@ class Product(models.Model):
         return discounted_price
 
 
-# class Variation(models.Model):
-#     pass
+class VariationManager(models.Manager):
+    def colors(self):
+        return super().filter(variation_category='color', is_active=True)
+    def sizes(self):
+        return super().filter(variation_category='size', is_active=True)
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=50, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
+
+# ToDo Review Rating and Products Gallery
